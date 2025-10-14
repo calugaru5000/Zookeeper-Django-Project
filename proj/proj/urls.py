b2ed15509741360d969790f0f81825176a418076
Paul-Gabriel <paul1111stanila@gmail.com>
2025-10-14 21:02:17 +0300

    Add initial migration and models for Species and Animal; update .gitignore and settings

--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('accounts/', include('django.contrib.auth.urls')),
-        path('', include('app.urls')),
+        path('', include('zookeeper.urls')),
       ]
 
 # proj/proj/urls.py
--- proj/zookeeper/models.py
+++ proj/zookeeper/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Species(models.Model):
     DIETS = [
         ("herbivore","Herbivore"),
